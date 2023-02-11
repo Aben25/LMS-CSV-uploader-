f93957bd2f33c9ceea452e6657be09ab9b19f884
Abenezer Nuro <anuro@artba.org>
2023-02-11 13:10:27 -0500

    first amplifyp

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1 className="text-3xl font-bold ">
-      LMS CSV uploader!
+      LMS CSV uploader.
       </h1>
       <FileUploader />
       <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" href="https://artba.sisense.com/app/main/dashboards/63e14901cde28c003326f83c">Open Dashboard</a>
