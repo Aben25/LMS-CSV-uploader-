298e3d69defc5c3d653e8ab497a51174b0184aa4
Abenezer Nuro <anuro@artba.org>
2023-02-28 12:10:09 -0500

    look fix

--- app/Dashboard.js
+++ app/Dashboard.js
@@ -2,7 +2,9 @@ function Dashboard() {
     return (
         <div>
             <h3 className="text-2xl font-bold p-3">Dashboard</h3>
-            <iframe width="100%" frameborder="0" src="https://artba.sisense.com/app/main/dashboards/63fd3153d50e0a003581db12?embed=true"></iframe>
+            <iframe width="100%" height="1000px" frameborder="0" src="https://artba.sisense.com/app/main/dashboards/63fd3153d50e0a003581db12?embed=true"></iframe>
+
+
         </div>
     );
 }
